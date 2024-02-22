bd05e79c84e241e76964cc6f00726ba27291cb82
ghrnwjd <hoyoung.wjd@gmail.com>
2024-02-22 17:42:11 +0900

    add: team moongegoorm project

---
+++ src/main/java/com/hufs/moongegoorm/controller/GoormController.java
@@ -0,0 +1,45 @@
+package com.hufs.moongegoorm.controller;
+
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+@RequiredArgsConstructor
+public class GoormController {
+
+      private final AmazonS3Client amazonS3Client;
+
+      @Value("${cloud.aws.s3.bucket}")
+      private String bucket;
+
+      @GetMapping("")
+      public String index() {
+            return "index.html";
+      }
+
+      @PostMapping("/upload")
+      public @ResponseBody ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+            try {
+                  String fileName= file.getOriginalFilename();
+                  String fileUrl= "https://" + bucket + "/test" +fileName;
+                  ObjectMetadata metadata= new ObjectMetadata();
+                  metadata.setContentType(file.getContentType());
+                  metadata.setContentLength(file.getSize());
+                  amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
+                  return ResponseEntity.ok(fileUrl);
+            } catch (IOException e) {
+                  e.printStackTrace();
+                  return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+      }
+}
---
+++ src/test/java/com/hufs/moongegoorm/MoongegoormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufs.moongegoorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoongegoormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
